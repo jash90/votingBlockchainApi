095404c2d77a3cf18609492d001226d7cd7c482a
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2018-08-04 13:39:27 +0200

    change db.sql

--- db.sql
+++ db.sql
@@ -14,11 +14,23 @@ CREATE DATABASE "votingOnlineDb"
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1;
 		
+CREATE TABLE "answer" (
+	"Id" serial NOT NULL,
+	"name" VARCHAR(50) NOT NULL,
+	"questionId" integer NOT NULL,
+	"userRole" integer NOT NULL DEFAULT '0',
+	CONSTRAINT answer_pk PRIMARY KEY ("Id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
 CREATE TABLE "user" (
 	"Id" serial NOT NULL,
 	"login" VARCHAR(50) NOT NULL UNIQUE,
 	"password" VARCHAR(64) NOT NULL,
-	"userRoleId" integer NOT NULL,
+	"userRoleId" integer NOT NULL DEFAULT '3',
 	CONSTRAINT user_pk PRIMARY KEY ("Id")
 ) WITH (
   OIDS=FALSE
@@ -29,6 +41,8 @@ CREATE TABLE "user" (
 CREATE TABLE "userRole" (
 	"Id" serial NOT NULL,
 	"name" VARCHAR(50) NOT NULL,
+	"login" BOOLEAN NOT NULL DEFAULT 'true',
+	"register" BOOLEAN NOT NULL DEFAULT 'true',
 	CONSTRAINT userRole_pk PRIMARY KEY ("Id")
 ) WITH (
   OIDS=FALSE
@@ -39,8 +53,9 @@ CREATE TABLE "userRole" (
 CREATE TABLE "question" (
 	"Id" serial NOT NULL,
 	"name" VARCHAR(100) NOT NULL,
-	"multiAnswer" BOOLEAN NOT NULL DEFAULT false,
-	"userRoleId" integer NOT NULL,
+	"multiAnswer" BOOLEAN NOT NULL DEFAULT 'false',
+	"userRoleId" integer NOT NULL DEFAULT '0',
+	"publicated" BOOLEAN NOT NULL DEFAULT 'false',
 	CONSTRAINT question_pk PRIMARY KEY ("Id")
 ) WITH (
   OIDS=FALSE
@@ -48,21 +63,9 @@ CREATE TABLE "question" (
 
 
 
-CREATE TABLE "answer" (
-	"Id" serial NOT NULL,
-	"name" VARCHAR(100) NOT NULL,
-	"questionId" integer NOT NULL,
-	"userRoleId" integer NOT NULL,
-	CONSTRAINT answer_pk PRIMARY KEY ("Id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
 CREATE TABLE "answerUser" (
-	"userId" integer NOT NULL,
-	"answerId" integer NOT NULL
+	"answerId" integer NOT NULL,
+	"userId" integer NOT NULL
 ) WITH (
   OIDS=FALSE
 );
@@ -71,8 +74,8 @@ CREATE TABLE "answerUser" (
 
 CREATE TABLE "token" (
 	"Id" serial NOT NULL,
-	"userId" integer NOT NULL,
-	"token" VARCHAR(64) NOT NULL,
+	"userId" integer NOT NULL UNIQUE,
+	"token" VARCHAR(64) NOT NULL UNIQUE,
 	CONSTRAINT token_pk PRIMARY KEY ("Id")
 ) WITH (
   OIDS=FALSE
@@ -80,18 +83,19 @@ CREATE TABLE "token" (
 
 
 
+ALTER TABLE "answer" ADD CONSTRAINT "answer_fk0" FOREIGN KEY ("questionId") REFERENCES "answer"("Id");
+ALTER TABLE "answer" ADD CONSTRAINT "answer_fk1" FOREIGN KEY ("userRole") REFERENCES "userRole"("Id");
+
 ALTER TABLE "user" ADD CONSTRAINT "user_fk0" FOREIGN KEY ("userRoleId") REFERENCES "userRole"("Id");
 
 
 ALTER TABLE "question" ADD CONSTRAINT "question_fk0" FOREIGN KEY ("userRoleId") REFERENCES "userRole"("Id");
 
-ALTER TABLE "answer" ADD CONSTRAINT "answer_fk0" FOREIGN KEY ("questionId") REFERENCES "question"("Id");
-ALTER TABLE "answer" ADD CONSTRAINT "answer_fk1" FOREIGN KEY ("userRoleId") REFERENCES "userRole"("Id");
-
-ALTER TABLE "answerUser" ADD CONSTRAINT "answerUser_fk0" FOREIGN KEY ("userId") REFERENCES "user"("Id");
-ALTER TABLE "answerUser" ADD CONSTRAINT "answerUser_fk1" FOREIGN KEY ("answerId") REFERENCES "answer"("Id");
+ALTER TABLE "answerUser" ADD CONSTRAINT "answerUser_fk0" FOREIGN KEY ("answerId") REFERENCES "answer"("Id");
+ALTER TABLE "answerUser" ADD CONSTRAINT "answerUser_fk1" FOREIGN KEY ("userId") REFERENCES "user"("Id");
 
 ALTER TABLE "token" ADD CONSTRAINT "token_fk0" FOREIGN KEY ("userId") REFERENCES "user"("Id");
+
 
 -- Create userRole
 
