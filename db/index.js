688d2f08cd2924bb471d634c7c3f279d78fb0390
Bartłomiej Zimny <bartekziimny90@gmail.com>
2018-08-04 15:36:06 +0200

    add authorization method

--- db/index.js
+++ db/index.js
@@ -1,12 +1,27 @@
-const { Pool } = require('pg')
-const pool = new Pool({
-  user: "admin",
-  host: "localhost",
-  database: "votingOnlineDb",
-  password: "admin",
-  port: 5432
-});
+const {Pool} = require('pg')
+const pool = new Pool({user: "admin", host: "localhost", database: "votingOnlineDb", password: "admin", port: 5432});
+
+const status = require("../status");
 
 module.exports = {
-    query: (text, params) => pool.query(text, params)
+  query: (text, params) => 
+    pool.query(text, params)
+  ,
+  auth: (token) => 
+    pool
+      .query('Select * FROM "token" where "token" = $1', [token])
+      .then(data =>{
+        if (data.rowCount == 1)
+        {
+          return { data: data.rows[0],status: status.OK.code, message: status.OK.message }
+        }
+        else
+        {
+          return { status: status.Unauthorized.code, message: status.Unauthorized.message }
+        }
+      })
+      .catch(error=>{
+        return {status : status.Error.code, message:error.detail}
+      })
+  
 }
--- routes/getQuestions.js
+++ routes/getQuestions.js
@@ -7,27 +7,22 @@ const crypto = require("crypto-js");
 
 router.post("/", (req, res) => {
     var token = req.body.token;
-
-    db.query('Select * FROM "token" where "token" = $1', [token])
-      .then((data)=> {
-        if (data.rowCount == 1)
-        {
-            db.query('Select * FROM "question"')
-                .then((data2) => {
-                    res.json({ data: data2,status: status.Ok.code, message: status.Ok.code });
-                })
-                .catch((error) => {
-                    res.json({ status: status.Error.code, message: error });
-                })
-        }
-        else
-        {
-            res.json({ status: status.Unauthorized.code, message: status.Unauthorized.message });
-        }
-      })
-      .catch(error => {
-        res.json({ status: status.Error.code, message: error });
-      });
+    db
+        .auth(token)
+        .then(response => {
+            if (response.status == 200) {
+                db
+                    .query('Select * FROM "question"')
+                    .then((data2) => {
+                        res.json({data: data2.rows, status: status.OK.code, message: status.OK.message});
+                    })
+                    .catch((error2) => {
+                        res.json({status: status.Error.code, message: error2.detail});
+                    })
+            } else {
+                res.json(response);
+            }
+        });
 });
 
 module.exports = router;
